b6ee5c1732d6d21f896275d30f2f65e1eb8a0d75
souleye NDIAYE <sulaynd@gmail.com>
2024-08-05 12:19:17 -0400

    first commit

---
+++ src/test/java/com/loulysoft/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loulysoft.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
